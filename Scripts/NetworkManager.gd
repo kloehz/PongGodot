b8d7f27d7c995f9b95fcc6c9530959fe3f71ce1c
Ludmila Diaz <kludmiladiaz@gmail.com>
2024-05-05 20:21:11 -0300

    rpc

--- Scripts/NetworkManager.gd
+++ Scripts/NetworkManager.gd
@@ -10,9 +10,9 @@ var PORT: int = 25565
 @onready var name_control = $CanvasLayer/NameControl
 @onready var connection_panel = $CanvasLayer/ConnectionPannel
 @onready var player_name_label = $CanvasLayer/NameControl/PlayerName
-@onready var players_ready_label = $CanvasLayer/ConnectionPannel/GridContainer/PlayersReady
-@onready var total_players_label = $CanvasLayer/ConnectionPannel/GridContainer/TotalPlayers
-@onready var ready_button = $CanvasLayer/ConnectionPannel/ReadyButton
+@onready var players_ready_label: Label = $CanvasLayer/ConnectionPannel/GridContainer/PlayersReady
+@onready var total_players_label: Label = $CanvasLayer/ConnectionPannel/GridContainer/TotalPlayers
+@onready var ready_button: Button = $CanvasLayer/ConnectionPannel/ReadyButton
 @onready var blue_team_grid: GridContainer = $CanvasLayer/ConnectionPannel/BlueTeam
 @onready var red_team_grid: GridContainer = $CanvasLayer/ConnectionPannel/RedTeam
 @onready var red_team_button = $CanvasLayer/ConnectionPannel/RedTeamButton
@@ -27,7 +27,7 @@ var new_ball_v2: CharacterBody2D;
 var players_count = 0
 var players_ready = 0
 var players_list = {}
-var is_ready = true
+var is_ready = false
 
 # TEMPORTAL VAR ENV
 const IS_SERVER = false
@@ -54,89 +54,154 @@ func start_game(id: int):
 		tempPlayer.player_name = player_name_label.text
 		tempPlayer.name = str(id)
 		tempPlayer.team_color_enum = Constants.TEAM_COLOR_ENUM.RED
-		#start_game(multiplayer.get_unique_id())
 		add_child(tempPlayer)
 
 func _on_ready_button_pressed():
-	players_list[multiplayer.get_unique_id()] = {
+	is_ready = !is_ready
+	players_list[str(multiplayer.get_unique_id())] = {
 		"is_ready": is_ready
 	}
-	is_ready = false
-	update_players_count(is_ready)
-
-@rpc("any_peer")
-func update_players_count(is_ready_rpc: bool):
-	if is_ready_rpc:
-		players_ready_label += 1
-		players_ready += 1
+	
+	if is_ready:
+		ready_button.add_theme_color_override("font_color", Color("#33ff30"))
+		rpc("_update_players_ready", 1, players_list)
 	else:
-		players_ready_label -= 1
-		players_ready -= 1
+		ready_button.add_theme_color_override("font_color", Color("#ff3730"))
+		rpc("_update_players_ready", -1, players_list)
 
 func _on_red_team_button_pressed():
 	red_team_button.disabled = true
 	blue_team_button.disabled = false
-	#_change_red_team_remote(multiplayer.get_unique_id(), player_name_label.text)
+	ready_button.disabled = false
+	_change_red_team_remote(multiplayer.get_unique_id(), player_name_label.text)
+	rpc("_update_players_state", players_list, players_count, players_ready)
 	rpc("_change_red_team_remote", multiplayer.get_unique_id(), player_name_label.text)
 
-@rpc("any_peer" )
+@rpc("any_peer")
 func _change_red_team_remote(player_id: int, player_name: String):
+	_remove_player_from_team(player_id, false)
 	var current_player = players_list.get(str(player_id))
-	#if current_player && current_player["team_color"] != Constants.TEAM_COLOR_ENUM.RED:
 	if current_player:
 		var grid_childrens = blue_team_grid.get_children()
 		for children in grid_childrens:
-			if children.text == players_list[str(player_id)]["player_name"]:
+			if children.text == player_name:
 				blue_team_grid.remove_child(children)
-		
 	players_list[str(player_id)] = {
 		"team_color": Constants.TEAM_COLOR_ENUM.RED,
-		"player_name": players_list[str(player_id)]["player_name"],
+		"player_name": player_name
 	}
 	var player_label = Label.new()
-	player_label.text = players_list[str(player_id)]["player_name"]
+	player_label.text = player_name
 	red_team_grid.add_child(player_label)
 
 func _on_blue_team_button_pressed():
 	red_team_button.disabled = false
 	blue_team_button.disabled = true
-	#_change_blue_team_remote(multiplayer.get_unique_id(), player_name_label.text)
+	ready_button.disabled = false
+	_change_blue_team_remote(multiplayer.get_unique_id(), player_name_label.text)
+	rpc("_update_players_state", players_list, players_count, players_ready)
 	rpc("_change_blue_team_remote", multiplayer.get_unique_id(), player_name_label.text)
 
 @rpc("any_peer")
 func _change_blue_team_remote(player_id: int, player_name: String):
+	_remove_player_from_team(player_id, true)
 	var current_player = players_list.get(str(player_id))
-	#if current_player && current_player["team_color"] != Constants.TEAM_COLOR_ENUM.BLUE:
 	if current_player:
 		var grid_childrens = red_team_grid.get_children()
 		for children in grid_childrens:
-			if children.text == players_list[str(player_id)]["player_name"]:
+			if children.text == player_name:
 				red_team_grid.remove_child(children)
-				
 	players_list[str(player_id)] = {
 		"team_color": Constants.TEAM_COLOR_ENUM.BLUE,
-		"player_name": players_list[str(player_id)]["player_name"],
+		"player_name": player_name
 	}
 	var player_label = Label.new()
-	player_label.text = player_name_label.text
+	player_label.text = player_name
 	blue_team_grid.add_child(player_label)
 
 # ----------------------------------- Player Counter UI -----------------------------------
 
 func peer_connected(id):
-	if IS_SERVER:
+	if !IS_SERVER:
 		return
-	rpc("_update_total_players_label", 1)
-	
+	players_count += 1
+	rpc("_update_players_state", players_list, players_count, players_ready)
+	print("players_list: ", players_list)
+	rpc_id(id, "_reconcile_data_connected", players_list)
+
 func peer_disconnected(id):
-	if IS_SERVER:
+	if !IS_SERVER:
 		return
-	rpc("_update_total_players_label", -1)
+	players_count += -1
+	if players_list[str(id)].get("is_ready"):
+		players_ready += -1
+	print("players list: ", players_list)
+	rpc("_remove_player_from_team", id, false, true)
+	rpc("_update_players_state", players_list, players_count, players_ready)
+	
+@rpc("any_peer", "call_local")
+func _update_players_ready(player_ready, players_list_rpc):
+	players_ready += player_ready
+	players_ready_label.text = str(players_ready)
+	players_list = players_list_rpc
+	
+@rpc("any_peer")
+func _remove_player_from_team(id: int, is_blue_team: bool, execute_both: bool = false):
+	var current_player = players_list.get(str(id))
+	print("players_list: ", players_list)
+	if !current_player:
+		return
+
+	if execute_both:
+		remote_blue_team(current_player)
+		remove_red_team(current_player)
+		return
+
+	if is_blue_team:
+		remote_blue_team(current_player)
+	else:
+		remove_red_team(current_player)
+
+# TODO: No deberia recorrer esto dos veces, con una es suficiente
+func remote_blue_team(current_player):
+	var grid_blue_childrens = blue_team_grid.get_children()
+	for children in grid_blue_childrens:
+		if children.text == current_player["player_name"]:
+			blue_team_grid.remove_child(children)
+
+func remove_red_team(current_player):
+	var grid_red_childrens = red_team_grid.get_children()
+	for children in grid_red_childrens:
+		if children.text == current_player["player_name"]:
+			red_team_grid.remove_child(children)
+ 
+@rpc("any_peer")
+func _reconcile_data_connected(players_list_reconcilier):
+	# Update players teams selected if have it
+	for key in players_list_reconcilier:
+		if key == str(multiplayer.get_unique_id()):
+			break
+		var current_player = players_list_reconcilier[str(key)]
+		if !current_player.get("team_color"):
+			return
+		if current_player["team_color"] == Constants.TEAM_COLOR_ENUM.BLUE:
+			var player_label = Label.new()
+			player_label.text = current_player["player_name"]
+			blue_team_grid.add_child(player_label)
+		if current_player["team_color"] == Constants.TEAM_COLOR_ENUM.RED:
+			var player_label = Label.new()
+			player_label.text = current_player["player_name"]
+			red_team_grid.add_child(player_label)
 
 @rpc("any_peer")
-func _update_total_players_label(count: int):
-	players_count += count
+func _update_players_state(players_list_reconcilier, players_count_remote, players_ready_remote):
+	# Update players state
+	players_list = players_list_reconcilier
+	players_count = players_count_remote
+	players_ready = players_ready_remote
+	
 	total_players_label.text = str(players_count)
+	players_ready_label.text = str(players_ready)
 
 # ----------------------------------- Game Functions -----------------------------------
 
@@ -148,21 +213,22 @@ func _ready():
 
 func check_winner_team():
 	if red_team_score == goal_score:
-		rpc("show_goal_text", Constants.TEAM_COLOR_ENUM.RED)
+		rpc ("show_goal_text", Constants.TEAM_COLOR_ENUM.RED)
 		show_goal_text(Constants.TEAM_COLOR_ENUM.RED)
 	if blue_team_score == goal_score:
-		rpc("show_goal_text", Constants.TEAM_COLOR_ENUM.BLUE)
+		rpc ("show_goal_text", Constants.TEAM_COLOR_ENUM.BLUE)
 		show_goal_text(Constants.TEAM_COLOR_ENUM.BLUE)
 
 func _physics_process(_delta):
-	if not multiplayer.is_server():
-		return
-	if multiplayer.get_peers().size() >= 1 && !hasBallSpawned:
-		new_ball = ball_scene.instantiate()
-		new_ball.position = get_viewport().get_visible_rect().size / 2
-		new_ball.name = "_Ball"
-		add_child(new_ball, true)
-		hasBallSpawned = true
+	pass
+	#if not multiplayer.is_server():
+		#return
+	#if multiplayer.get_peers().size() >= 1&&!hasBallSpawned:
+		#new_ball = ball_scene.instantiate()
+		#new_ball.position = get_viewport().get_visible_rect().size / 2
+		#new_ball.name = "_Ball"
+		#add_child(new_ball, true)
+		#hasBallSpawned = true
 
 @rpc("any_peer")
 func show_goal_text(team: int):
@@ -179,4 +245,5 @@ func _is_server_connection():
 		print("Server creation failed")
 		return
 	multiplayer.multiplayer_peer = peer
-	peer.peer_connected.connect(start_game)
+	#peer.peer_connected.connect(start_game)
+	print("Server connection success")
