92cc8c3cc85c606f2d6a1110f2553fd536537d1e
Ludmila Diaz <kludmiladiaz@gmail.com>
2024-07-06 20:59:01 -0300

    fix joystick

--- Scripts/NetworkManager.gd
+++ Scripts/NetworkManager.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var PORT: int = 25565#OS.get_environment("GODOT_PORT").to_int() # 25565
 var IP_REMOTE: String = "154.49.246.149"#OS.get_environment("GODOT_IP") # "154.49.246.149"
-var IS_SERVER: int = 0#OS.get_environment("GODOT_SERVER").to_int() # 0 - 1
+var IS_SERVER: int = 0 #OS.get_environment("GODOT_SERVER").to_int() # 0 - 1
 
 var blue_player_scene: PackedScene = preload("res://Scenes/player_blue.tscn")
 var red_player_scene: PackedScene = preload("res://Scenes/player_red.tscn")
@@ -70,8 +70,8 @@ func add_new_player(player_id):
 	# here we need to spawn players on side
 	player.team_color_enum = players_list[player_id]["team_color"]
 	add_child(player, true)
-	if OS.get_name() == "Android":
-		$Joystick.show()
+	if OS.get_name() != "Android":
+		$Joystick/UI.show()
 	connection_panel.hide()
 
 func get_blue_random_position() -> Vector2:
